3f05c1dde980fee7385f1f4b5e9dea39c5bd42ae
Will Morgan <wmorganjr@gmail.com>
2013-09-28 18:45:10 -0500

    Add clock model to MessageQueue

--- resources/public/js/messages.js
+++ resources/public/js/messages.js
@@ -1,4 +1,10 @@
 (function($) {
+  var Clock = Backbone.Model.extend({
+    millisecondsRemaining: function() {
+      return this.get('deadline') - this.get('current');
+    }
+  });
+
   var Message = Backbone.Model.extend({
     getChatString: function() {
       return 'Message id ' + this.get('id') + ': Time: ' + this.get('server-time-unix-millis');
@@ -26,12 +32,18 @@
       'isStopped': false,
       'roomId': 'room-a',
       'pollInterval': 2500,
-      'handlerFunction': function() {}
+      'handlerFunction': function() {},
+      'tickFunction': function() {}
     },
 
     initialize: function() {
       this.set('messages', new Messages());
       this.get('messages').on('add', this.handleNewMessage, this);
+
+      this.set('clock', new Clock({
+        deadline: Date.now() + 5 * 60 * 1000
+      }));
+      this.get('clock').on('change', this.callTickFunction, this);
     },
 
     start: function() {
@@ -50,7 +62,7 @@
       }
       var params = this.get('messages').isEmpty() ? {} : {since: this.latestMessageId()};
 
-      $.getJSON(this.messagesPath(), params, _.bind(this.addMessages, this));
+      $.getJSON(this.messagesPath(), params, _.bind(this.handleResponse, this));
 
       setTimeout(_.bind(this.poll, this), this.get('pollInterval'));
     },
@@ -63,13 +75,19 @@
       return '/rooms/' + this.get('roomId') + '/messages';
     },
 
-    addMessages: function(messages) {
-      this.get('messages').add(messages);
+    handleResponse: function(response) {
+      this.get('clock').set('current', response['server-time-unix-millis']);
+      this.get('messages').add(response.messages);
     },
 
     handleNewMessage: function(message) {
       this.get('handlerFunction').call(null, message);
-    }
+    },
+
+    callTickFunction: function() {
+      console.log(this.get('clock').millisecondsRemaining());
+      this.get('tickFunction').call(null, this.get('clock').millisecondsRemaining());
+    },
   });
 
   window.MessageQueue = MessageQueue;
