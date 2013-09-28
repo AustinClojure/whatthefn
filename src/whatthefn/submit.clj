8ae934b17432bb0bce39a04f41acc07486b6dad0
Norman Richards <orb@nostacktrace.com>
2013-09-27 22:08:26 -0500

    improve code eval

--- src/whatthefn/submit.clj
+++ src/whatthefn/submit.clj
@@ -6,6 +6,24 @@
   {:body (pr-str data)
    :headers {"Content-Type" "application/edn;charset=UTF-8"}})
 
+(defn str-reader [text]
+  (java.io.PushbackReader. (java.io.StringReader. text)))
+
+(defn edn-seq [reader]
+  (when-let [next-val (clojure.edn/read {:eof nil} reader)]
+    (cons next-val (edn-seq reader))))
+
+
+(defn eval-code [code]
+  (let [sandbox (sb/sandbox)
+        forms (edn-seq (str-reader code))
+        results (map sandbox forms)
+        result-text (clojure.string/join "\n" (map pr-str results))]
+    result-text))
+
+
 (defn submit-fn [code]
-  (let [result  ((sb/sandbox) (clojure.edn/read-string code))]
-    (edn-response {:result result})))
+  (try
+    (edn-response {:result (eval-code code)})
+    (catch Exception e
+      (edn-response {:result (.getMessage e)}))))
