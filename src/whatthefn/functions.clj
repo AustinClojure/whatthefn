f015d00134a0ce668df5300cda31078eedf74d1a
dks <strawndk@gmail.com>
2013-09-29 16:46:58 -0500

    added functions

--- src/whatthefn/functions.clj
+++ src/whatthefn/functions.clj
@@ -68,7 +68,9 @@
     ["what the fn!" :numeric our-name "7th function" #{0 1 2 3 4 5 6 7 8 9 10 11 12}]
     ["follow the 1 through the trail!!!" :numeric follow-the "8th function" #{1 99 200 700}]
     ["prime?" :numeric prime? "9th function" #{1 2 16 7 23 8}]
-    ["the even part" :numeric-collection even-only "10th function" #{#{1 2 3 4} #{3 4 5 19 20 1}}]})
+    ["the even part" :numeric-collection even-only "10th function" #{#{1 2 3 4} #{3 4 5 19 20 1}}]
+    ["stop yelling" :string clojure.string/upper-case "for the lulz" #{"why oh why" "wHY oh W" "ssacc1"}]
+    ["try a sentence! how verbose are you?" :string #(clojure.string/split % #"\ ") "for the lols" #{"why oh why" "why why" "why"}]})
 
 (defn plus2 [n]
   (+ n 2))
