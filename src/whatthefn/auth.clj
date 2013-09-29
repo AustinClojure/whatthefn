3f5941361af332119cc38f9ee8e81d781dc2c440
Norman Richards <orb@nostacktrace.com>
2013-09-29 16:01:41 -0500

    improve registration

--- src/whatthefn/auth.clj
+++ src/whatthefn/auth.clj
@@ -42,6 +42,24 @@
     :session {}))
 
 
+
+(defn validate-new-user [username password]
+  (cond
+   (not (<= 3 (count username) 16))
+   "Username must be between 3 and 16 characters"
+
+   (not (re-matches #"[a-zA-Z0-9]+" username))
+   "Username must contain letters and digits only."
+
+   (< (count password) 6)
+   "Passwords should be at least 6 characters."
+
+   :else
+   nil))
+
+
 (defn register [req username password]
-  (add-user-password username password)
-  (login req username password))
+  (if-let [error-response (validate-new-user username password)]
+    (err/error-page 400 error-response)
+    (do (add-user-password username password)
+        (login req username password))))
