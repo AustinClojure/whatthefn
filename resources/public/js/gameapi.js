48d63bf8b405eed11a612051b1bc5ad4173d6105
dks <strawndk@gmail.com>
2013-09-29 01:40:43 -0500

    expanded button functionality in gameapi.js and chat.html

--- resources/public/js/gameapi.js
+++ resources/public/js/gameapi.js
@@ -1,23 +1,32 @@
 (function($) {
   var GameApi = Backbone.Model.extend({
     defaults: {
-      'roomId': 'room-a',
-      'playerId': 'player-a'
+      'roomId': 'the-room',
+      'playerId': 'newbie'
     },
 
     resolveInput: function(input) {
       this.sendEvent({
         type: 'resolve-input',
         player: this.get('playerId'),
+        room: this.get('roomId'),
         arg: input
       });
     },
 
     testSolution: function(solution) {
       this.sendEvent({
-        type: 'resolve-input',
+        type: 'test-solution',
         player: this.get('playerId'),
         'function': solution
+      });
+    },
+    
+    leave: function(input) {
+      this.sendEvent({
+	type: 'player-left',
+	player: this.get('playerId'),
+        room: this.get('roomId')
       });
     },
 
